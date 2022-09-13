4acb3e9208a7c3d11ea791b54b94b5fe67ffc998
Michael <michael.philipe2013@gmail.com>
2022-09-13 00:33:30 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato("cat", "black", 5);
+        System.out.println(gato);
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+}
